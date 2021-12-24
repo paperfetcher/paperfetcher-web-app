1d55a27ac6ca2541d499712369b367ac3a14d276
Akash Pallath <pallathakash@gmail.com>
2021-12-24 16:34:16 -0500

    UI updates

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-# @author Akash Pallath
+# @author Akash Pallath\
 
-import streamlit as st
+import datetime
+
 import pandas as pd
+import streamlit as st
+
+import time
 
 ################################################################################
 # Supporting functions
@@ -41,6 +45,8 @@ with st.expander("Need help?"):
                   To learn more about PubMed, visit [PubMed.gov](https://pubmed.ncbi.nlm.nih.gov)
                 """)
 
+st.markdown("---")
+
 # Section 2
 
 st.header("2. What type of search do you want to perform?")
@@ -53,24 +59,94 @@ with st.expander("What's this?"):
                 - **Snowball-search**:
                 """)
 
+st.markdown("---")
+
 # Section 3
 
 # Crossref handsearch
 if db == "Crossref" and search == "Handsearch":
     st.header("3. Define your handsearch parameters.")
 
-    # Journal ISSN
-    journal_list = load_crossref_journals_dict()
-    option = st.selectbox("Search for a journal.", journal_list, index=50000)
+    # Journals
+    st.subheader("a) Select journals to search in.")
+    st.write("You can add multiple journals to a single handsearch.")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        journal_list = load_crossref_journals_dict()
+        option = st.selectbox("Search for a journal.",
+                              journal_list,
+                              index=50000,
+                              help="""Type the name of a journal to search for it in the database.
+                                    This might be slow due to a large number of entries.
+                                    Once you find the journal, click on the 'Add to Search' button.""")
+
+        if st.button("Add to search", key="cr_hs_journal"):
+            if 'selected_journals_list' not in st.session_state:
+                st.session_state.selected_journals_list = [option]
+            else:
+                st.session_state.selected_journals_list.append(option)
+
+    with col2:
+        issn = st.text_input("Enter an ISSN",
+                             help="""If you the know the ISSN of a journal you wish to search within,
+                                     you can type it here and click on the 'Add to Search' button.
+                                     If the journal has a print ISSN and an electronic ISSN, use the electronic ISSN.""")
+
+        if st.button("Add to search", key="cr_hs_issn"):
+            if 'selected_journals_list' not in st.session_state:
+                st.session_state.selected_journals_list = [issn]
+            else:
+                st.session_state.selected_journals_list.append(issn)
+
+    issn_list = st.multiselect("Selected journals to search in",
+                               options=st.session_state.selected_journals_list,
+                               default=st.session_state.selected_journals_list)
+
+    # Start and end date
+    st.subheader("b) Select a date range to fetch articles within.")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        start = st.date_input("Fetch from this date onwards.",
+                              min_value=datetime.date(1900, 1, 1),
+                              max_value=datetime.date.today())
+
+    with col2:
+        end = st.date_input("Fetch until this date.",
+                            min_value=datetime.date(1900, 1, 1),
+                            max_value=datetime.date.today())
+
+    # Start and end date
+    st.subheader("c) Enter search keywords (optional).")
+
+    keywords = st.text_area(label="Enter comma-separated keywords")
+
+    # Search button
+    st.subheader("d) Perform search")
+    if st.button("Search"):
+        my_bar = st.progress(0)
+
+        for percent_complete in range(100):
+            time.sleep(0.1)
+            my_bar.progress(percent_complete + 1)
+
+        st.markdown("---")
+
+        # View and save results
+        st.header("4. View and save search results")
+
 
 # Crossref snowballsearch
 if db == "Crossref" and search == "Snowball-search":
     st.header("3. Define your snowball-search parameters.")
 
-# Pubmed handsearch
+# PubMed handsearch
 if db == "PubMed" and search == "Handsearch":
     st.header("3. Define your handsearch parameters.")
 
-# Pubmed snowballsearch
+# PubMed snowballsearch
 if db == "PubMed" and search == "Snowball-search":
     st.header("3. Define your snowball-search parameters.")
